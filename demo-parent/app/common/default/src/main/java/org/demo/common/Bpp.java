6758abdf1325e34e6b54c8659aded1ad425e9e11
YangjianForWork <yangjianfw@163.com>
2016-07-21 13:56:21 +0800

    增加2

---
+++ demo-parent/app/common/default/src/main/java/org/demo/common/Bpp.java
@@ -0,0 +1,14 @@
+package org.demo.common;
+
+public class Bpp {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
