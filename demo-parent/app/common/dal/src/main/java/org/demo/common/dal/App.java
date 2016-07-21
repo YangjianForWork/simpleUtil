439a90072bbd1e46bfb53b28c54f037b4e5c4623
YangjianForWork <yangjianfw@163.com>
2016-07-21 14:11:40 +0800

    Test1607211411

--- demo-parent/app/common/dal/src/main/java/org/demo/common/dal/App.java
+++ demo-parent/app/common/dal/src/main/java/org/demo/common/dal/App.java
@@ -1,5 +1,7 @@
 package org.demo.common.dal;
 
+import org.demo.common.Bpp;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Bpp bpp = new Bpp();
+        bpp.setName("a");
     }
 }
