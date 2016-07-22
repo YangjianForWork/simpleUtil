9d9bd5e00fa15e4c03158b9e3ad2e20c983b5145
YangjianForWork <yangjianfw@163.com>
2016-07-22 09:39:31 +0800

    增加4

---
+++ demo-parent/app/biz/prod/src/main/java/org/demo/biz/prod/App.java
@@ -0,0 +1,13 @@
+package org.demo.biz.prod;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
